8d7cb14f16174313db811c9c3dab80013282a9d6
maeez <mansurimaeez@gmail.com>
2026-03-10 15:26:02 +0530

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Analytics } from "@vercel/analytics/react"
 import theme from './theme'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
         <ReactQueryDevtools />
+        <Analytics />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
