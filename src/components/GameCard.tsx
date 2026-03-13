5cb3832ee15cd3febce274f4da0264ebb8f20f82
maeez <mansurimaeez@gmail.com>
2026-03-13 11:50:49 +0530

    make game cards clickable to detail page

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -18,23 +18,25 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent="space-between" marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms?.map(
-              (p) => p.platform
-            )}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading fontSize="2xl">
-          <Link to={'/games/' + game.slug}>{game.name}</Link>
-          
-        </Heading>
-      </CardBody>
-    </Card>
+    <Link to={'/games/' + game.slug}>
+      <Card>
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms?.map(
+                (p) => p.platform
+              )}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize="2xl">
+            {game.name}
+            
+          </Heading>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
